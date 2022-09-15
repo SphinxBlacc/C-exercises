36fd9c9d7ee93cce3a09fdccc5ac557199e4abb0
SphinxBlacc <86077071+SphinxBlacc@users.noreply.github.com>
2022-09-15 18:48:43 -0400

    Add files via upload

---
+++ LeapYearTest/LeapYearTest/Source.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+
+	int year;
+
+	printf("Please enter a year: ");
+	scanf_s("%d", &year);
+
+	if (year % 4 == 0 && year % 100 != 0) {
+		printf("%d is a leap year.", year);
+	}
+	else if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0) {
+		printf("%d is a leap year.", year);
+	}
+	else {
+		printf("%d is not a leap year.", year);
+	}
+
+	return 0;
+}
